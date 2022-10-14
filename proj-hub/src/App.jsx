c4e4e48f34e02a4a4b226c28fba785e660600f3a
Akash Sinha <aksinhaemail.com>
2022-10-14 11:51:07 +0530

    Testing some stuff not important

--- proj-hub/src/App.jsx
+++ proj-hub/src/App.jsx
@@ -1,8 +1,10 @@
+import React from "react";
+
 function App() {
 
   return (
     <div className="h-screen w-screen grid items-center justify-center bg-lime-400">
-	<div className="font-bold text-blue-600 ">
+	<div className="font-bold text-blue-600">
 		Proj HUB
 	  </div>
 	</div>
